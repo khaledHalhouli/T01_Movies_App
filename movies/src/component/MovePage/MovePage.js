29842b25d49ab9fae69ad1ac6822a393d69621f8
khaledHalhouli <101004067+khaledHalhouli@users.noreply.github.com>
2022-07-05 23:06:29 +0300

    finish fav button

--- movies/src/component/MovePage/MovePage.js
+++ movies/src/component/MovePage/MovePage.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { React, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./style.css"
+import "./style.css";
+import { AiFillHeart } from "react-icons/ai";
 
 const MovePage = () => {
   const [move, setMove] = useState("");
+  const [favIcon, setfavIcon] = useState("iconFavNo");
   const { id } = useParams();
 
   useEffect(() => {
@@ -16,27 +18,92 @@ const MovePage = () => {
       .then((result) => {
         setMove(result.data);
         console.log(result.data);
-        
       })
       .catch((err) => {
         console.log(err);
       });
   }, [id]);
+  useEffect(() => {
+    if (
+      localStorage.getItem("favorite") &&
+      JSON.parse(localStorage.getItem("favorite")).includes(move.id)
+    ) {
+      setfavIcon("iconFavYes");
+    }
+    console.log(2);
+  }, [move]);
+  const favoriteButton = () => {
+    if (
+      !localStorage.getItem("favorite") ||
+      !JSON.parse(localStorage.getItem("favorite")).includes(move.id)
+    ) {
+      console.log(
+        !JSON.parse(localStorage.getItem("favorite")).includes(move.id)
+      );
+      let moveFavArr = localStorage.getItem("favorite")
+        ? JSON.parse(localStorage.getItem("favorite"))
+        : [];
+      localStorage.setItem(
+        "favorite",
+        JSON.stringify([...moveFavArr, move.id])
+      );
+      setfavIcon("iconFavYes");
+    } else {
+      let moveFavArr = JSON.parse(localStorage.getItem("favorite")).filter(
+        (element) => {
+          return element !== move.id;
+        }
+      );
+      localStorage.setItem("favorite", JSON.stringify(moveFavArr));
 
-  return <div className="movePage">
-    {move&&(<div className="move" style={{backgroundImage:`url(https://image.tmdb.org/t/p/w500${move.backdrop_path})`} }><div className="moveCard">
-        <img className="moveImg" src={`https://image.tmdb.org/t/p/w500${move.poster_path}`} alt={`${move.original_title}`}/>
-        <div className="moveInfo">
-        <h1 className="moveTitle">{move.original_title}</h1>
-        <p className="moveOverView">{move.overview}</p>
-        <p className="moveRate">{move.vote_average}/10</p>
-        <p className="moveDur">{Math.floor( move.runtime/60)} hr {Math.ceil((move.runtime/60-Math.floor( move.runtime/60))*60)} min</p>
-        <div className="moveGenres">{move.genres.map((element,index)=>{
-            return <p key={index} className="genrsName">{element.name}</p>
-        })}</div>
+      setfavIcon("iconFavNo");
+    }
+  };
+
+  return (
+    <div className="movePage">
+      {move && (
+        <div
+          className="move"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/w500${move.backdrop_path})`,
+          }}
+        >
+          <div className="moveCard">
+            <img
+              className="moveImg"
+              src={`https://image.tmdb.org/t/p/w500${move.poster_path}`}
+              alt={`${move.original_title}`}
+            />
+            <div className="moveInfo">
+              <h1 className="moveTitle">{move.original_title}</h1>
+              <p className="moveOverView">{move.overview}</p>
+              <p className="moveRate">{move.vote_average}/10</p>
+              <p className="moveDur">
+                {Math.floor(move.runtime / 60)} hr{" "}
+                {Math.ceil(
+                  (move.runtime / 60 - Math.floor(move.runtime / 60)) * 60
+                )}{" "}
+                min
+              </p>
+              <div className="moveGenres">
+                {move.genres.map((element, index) => {
+                  return (
+                    <p key={index} className="genrsName">
+                      {element.name}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
+          <p className={`${favIcon}`} onClick={favoriteButton}>
+            <AiFillHeart />
+          </p>
+          </div>{" "}
         </div>
-    </div> <button>favorite</button></div>)}
-  </div>
+      )}
+    </div>
+  );
 };
 
 export default MovePage;
